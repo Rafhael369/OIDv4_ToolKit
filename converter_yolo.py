7efd912232ca4409331183cc60e0381eb5543853
Rafhael369 <rafhael369@gmail.com>
2023-08-02 13:55:00 -0300

    Att repo

--- converter_yolo.py
+++ converter_yolo.py
@@ -2,14 +2,17 @@ import pandas as pd
 import os
 from glob import glob
 
+
 def get_annotations_dataframe(annotations_bbox, class_id, file_name):
     try:
         # the same file can be used for another type of class,
         # this filter find the file of our class
-        filter = annotations_bbox.ImageID.eq(file_name) & annotations_bbox.LabelName.eq(class_id)
+        filter = annotations_bbox.ImageID.eq(
+            file_name) & annotations_bbox.LabelName.eq(class_id)
         return annotations_bbox.loc[filter]
     except:
         raise Exception('error get annotations dataframe!')
+
 
 def get_class_id(class_descriptions_boxable, class_name):
     class_filter = class_descriptions_boxable['Tortoise'].isin([class_name])
@@ -18,6 +21,7 @@ def get_class_id(class_descriptions_boxable, class_name):
         return filtered_dataframe.to_numpy()[0][0]
     except:
         raise Exception('{} not exists!'.format(class_name))
+
 
 def create_darknet_annotation(df, file_name, class_name_index, output_dir):
     print('\n===== {}'.format(file_name))
@@ -32,20 +36,25 @@ def create_darknet_annotation(df, file_name, class_name_index, output_dir):
             w = (x_values[1] - x_values[0])
             h = (y_values[1] - y_values[0])
 
-            print("{} {} {} {} {}".format(class_name_index, center_x, center_y, w, h))
-            txt_file.write("{} {} {} {} {}\n".format(class_name_index, center_x, center_y, w, h))
+            print("{} {} {} {} {}".format(
+                class_name_index, center_x, center_y, w, h))
+            txt_file.write("{} {} {} {} {}\n".format(
+                class_name_index, center_x, center_y, w, h))
 
 
 # load annotations bounding boxes
-train_annotations_bbox = pd.read_csv('OID/csv_folder/train-annotations-bbox.csv')
+train_annotations_bbox = pd.read_csv(
+    'OID/csv_folder/train-annotations-bbox.csv')
 
 # load classes csv file
-class_descriptions_boxable = pd.read_csv('OID/csv_folder/class-descriptions-boxable.csv')
+class_descriptions_boxable = pd.read_csv(
+    'OID/csv_folder/class-descriptions-boxable.csv')
 
 for (class_name_index, class_name) in enumerate(sorted(os.listdir('OID/Dataset/train'))):
     class_id = get_class_id(class_descriptions_boxable, class_name)
     for img_file in glob('OID/Dataset/train/{}/*.jpg'.format(class_name)):
         file_name = os.path.basename(img_file).split('.')[0]
         file_dir = os.path.dirname(img_file)
-        df = get_annotations_dataframe(train_annotations_bbox, class_id, file_name)
+        df = get_annotations_dataframe(
+            train_annotations_bbox, class_id, file_name)
         create_darknet_annotation(df, file_name, class_name_index, file_dir)
--- enumerar_classes.py
+++ enumerar_classes.py
@@ -1,11 +1,40 @@
 import os
-import subprocess
 
-pasta = "/home/rafhael/Projetos/darknet/treinamento4/obj/obj/validation"
 
-for diretorio, subpastas, arquivos in os.walk(pasta):
-    for arquivo in arquivos:
-        if arquivo[-1] == 't':
-            caminho = os.path.join(diretorio, arquivo)
-            subprocess.call([f"sed -i -e 's/^.//' {caminho}"], shell=True)
-            subprocess.call([f"sed -i -e 's/^/1/' {caminho}"], shell=True)
+def replace_numbers_in_lines(file_path, replacements):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    with open(file_path, 'w') as file:
+        for line in lines:
+            words = line.split()
+            if words:
+                try:
+                    first_word = int(words[0])
+                    if first_word in replacements:
+                        replacement_number = replacements[first_word]
+                        line = line.replace(
+                            str(first_word), str(replacement_number), 1)
+                except ValueError:
+                    pass
+            file.write(line)
+
+
+def main(directory_path, replacements):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(directory_path, filename)
+            replace_numbers_in_lines(file_path, replacements)
+
+
+if __name__ == "__main__":
+    # Substitua pelo caminho do diretório que contém os arquivos .txt
+    directory_path = "/home/rafhael/Projetos/SMA/mi-sma-testes/OIDv4_ToolKit/OID/Dataset/teste"
+
+    # Substitua os números que deseja alterar pelos números correspondentes
+    replacements = {
+        0: 8,
+        # Adicione mais pares de substituição conforme necessário
+    }
+
+    main(directory_path, replacements)
